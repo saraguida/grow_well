424d864c1bddba37ec6c3a57befcd2061db20304
Alessia Nepitali <alessia.nepitali@gmail.com>
2023-07-01 22:04:43 +0200

    [main] Shared Preferences in the Profile Page

--- grow_well/lib/screens/ProfilePage.dart
+++ grow_well/lib/screens/ProfilePage.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/cupertino.dart';
-
+import 'package:intl/intl.dart';
 import 'package:grow_well/screens/LoginPage.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+final List<String> items = ['Female', 'Male', 'Other'];
+String? selectedValue;
+DateTime date = DateTime(2000, 1, 1);
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -22,11 +26,88 @@ class _ProfilePageState extends State<ProfilePage> {
   TextEditingController surnameController = TextEditingController();
   TextEditingController addressController = TextEditingController();
   TextEditingController phoneController = TextEditingController();
+  
+  
+  @override
+  void initState() {
+    super.initState();
+    //Check if the user has already inserted profile data
+    _checkLogin();
+  } //initState
 
-  DateTime date = DateTime(2000, 1, 1);
+  void _checkLogin() async {
+    //Get the SharedPreference instance and check if the values are set or not
+    final sp = await SharedPreferences.getInstance();
+    if (sp.getString('name') != null) {
+      nameController.text = sp.getString('name')!;
+    } //name check
+    if (sp.getString('surname') != null) {
+      surnameController.text = sp.getString('surname')!;
+    } //surname check
+    if (sp.getString('gender') != null) {
+      selectedValue = sp.getString('gender')!;
+      setState(() {
+        selectedValue = selectedValue;
+      });
+    } //gender check
+    if (sp.getString('date') == null) {
+      DateTime dateT = DateTime(2000, 1, 1);
+      String stringDate=date.toString();
+      sp.setString('date', stringDate);
+      setState(() {
+        date = dateT;
+      });
+    } else {
+      String dateString = sp.getString('date')!;
+      DateTime dateT = DateTime.parse(dateString);
+      setState(() {
+        date = dateT;
+      });
+    } //date of birth check
+    if (sp.getString('address') != null) {
+      addressController.text = sp.getString('address')!;
+    } //address check
+    if (sp.getString('phone') != null) {
+      phoneController.text = sp.getString('phone')!;
+    } //phone number check
+  } //_checkLogin
 
-  void _showDialog(Widget child) {
-    showCupertinoModalPopup<void>(
+  void _saveName(TextEditingController controller) async {
+    final sp = await SharedPreferences.getInstance();
+    sp.setString('name', controller.text);
+    } //_saveName
+
+  void _saveSurname(TextEditingController controller) async {
+    final sp = await SharedPreferences.getInstance();
+    sp.setString('surname', controller.text);
+    } //_saveSurname
+
+  void _saveGender(String value) async {
+    final sp = await SharedPreferences.getInstance();
+    sp.setString('gender', value);
+    } //_saveGender
+
+  void _saveDate(DateTime datetime) async {
+    String dateString = datetime.toString();
+    final sp = await SharedPreferences.getInstance();
+    await sp.setString('date', dateString);
+    setState(() {
+      date = datetime;
+    });
+    } //_saveDate
+
+  void _saveAddress(TextEditingController controller) async {
+    final sp = await SharedPreferences.getInstance();
+    sp.setString('address', controller.text);
+    } //_saveAddress
+
+  void _savePhone(TextEditingController controller) async {
+    final sp = await SharedPreferences.getInstance();
+    sp.setString('phone', controller.text);
+    } //_savePhone
+
+  Future<void> _showDialog(Widget child) async {
+  await showCupertinoModalPopup<void>(
       context: context,
       builder: (BuildContext context) => Container(
         height: 216,
@@ -48,7 +129,7 @@ class _ProfilePageState extends State<ProfilePage> {
   }
 
   @override
-  Widget build(BuildContext context) {
+ Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
           padding: EdgeInsets.all(15),
@@ -63,6 +144,7 @@ class _ProfilePageState extends State<ProfilePage> {
                     labelText: 'Name',
                     hintText: 'Enter Your Name',
                   ),
+                  onChanged: (text) {_saveName(nameController);}
                 ),
               ),
               Padding(
@@ -74,6 +156,7 @@ class _ProfilePageState extends State<ProfilePage> {
                     labelText: 'Surname',
                     hintText: 'Enter Your Surname',
                   ),
+                  onChanged: (text) {_saveSurname(surnameController);}
                 ),
               ),
               Padding(
@@ -104,6 +187,7 @@ class _ProfilePageState extends State<ProfilePage> {
                             setState(() {
                               selectedValue = value as String;
                             });
+                            _saveGender(selectedValue!);
                           },
                           buttonStyleData: const ButtonStyleData(
                             height: 40,
@@ -128,12 +212,14 @@ class _ProfilePageState extends State<ProfilePage> {
                             // This is called when the user changes the date.
                             onDateTimeChanged: (DateTime newDate) {
                               setState(() => date = newDate);
+                              _saveDate(date);
                             },
                           ),
                         ),
                         // In this example, the date is formatted manually. You can
                         // use the intl package to format the value based on the
                         // user's locale settings.
+
                         child: Text(
                           '${date.month}-${date.day}-${date.year}',
 //                                  style: const TextStyle(
@@ -154,6 +240,7 @@ class _ProfilePageState extends State<ProfilePage> {
                     labelText: 'Address',
                     hintText: 'Enter Your Address',
                   ),
+                  onChanged: (text) {_saveAddress(addressController);}
                 ),
               ),
               Padding(
@@ -165,6 +252,7 @@ class _ProfilePageState extends State<ProfilePage> {
                     labelText: 'Phone Number',
                     hintText: 'Enter Your Phone Number',
                   ),
+                  onChanged: (text) {_savePhone(phoneController);}
                 ),
               ),
             ],
@@ -188,5 +276,4 @@ class _ProfilePageState extends State<ProfilePage> {
   } //_toLoginPage
 }
 
-final List<String> items = ['Female', 'Male', 'Other'];
-String? selectedValue;
+
